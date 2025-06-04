8dcb56ff57e0ba59e9ae4068fff7de141e6d8535
Tim MacKay <simplytim42@gmail.com>
2025-06-04 09:03:21 +0100

    add cors and album art access

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-import os
+from fastapi.middleware.cors import CORSMiddleware
 
 from .dependencies import lifespan
 from .routers import artists, albums, filter, songs
@@ -8,13 +8,21 @@ from .routers import artists, albums, filter, songs
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(artists.router)
 app.include_router(albums.router)
 app.include_router(songs.router)
 app.include_router(filter.router)
 
-music_dir = os.path.abspath("music")
-app.mount("/song_files", StaticFiles(directory=music_dir), name="song_files")
+app.mount("/song_files", StaticFiles(directory="music"), name="song_files")
+app.mount("/album_art", StaticFiles(directory="art"), name="album_art")
 
 
 @app.get("/")
